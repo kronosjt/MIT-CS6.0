f4a9c616d16e9aba3a2fae1f902c43dcf9310849
kronosjt <jithin10a@gmail.com>
2016-12-07 16:05:53 +0530

    Some minor adjustments to coding format (PEP-8) Add template for Problem 4 in the assignment

--- ps4a.py
+++ ps4a.py
@@ -8,8 +8,10 @@ list, whose values are the size of your retirement account at the end of each ye
 most recent year's value at the end of the list.
 Complete the implementation of:
 def nestEggFixed (salary, save, growthRate, years):
+"""
 
-Retirement fund
+"""
+Formulae for calculation:
 End of year 1 F[0] = salary * save * 0.01
 End of year 2 F[1] = F[0] * (1 + 0.01 * growthRate) + salary * save * 0.01
 End of year 3 F[2] = F[1] * (1 + 0.01 * growthRate) + salary * save * 0.01
@@ -18,6 +20,8 @@ End of year 3 F[2] = F[1] * (1 + 0.01 * growthRate) + salary * save * 0.01
 #
 # Problem 1
 #
+
+
 def nestEggFixed(salary, save, growthRate, years):
     """
     - salary: the amount of money you make each year.
@@ -29,12 +33,13 @@ def nestEggFixed(salary, save, growthRate, years):
     - return: a list whose values are the size of your retirement account at
       the end of each year.
     """
-    savings = [] # List to save results
-    savings.append(salary * save * 0.01)
+    savings = list([])  # List to save results
+    savings.append(salary * save * 0.01) # Add F[0]
     while len(savings) < years:
         for i in range(1, years):
             savings.append(savings[i-1] * (1 + 0.01 * growthRate) + salary * save * 0.01)
     return savings
+
 
 def testNestEggFixed():
     savingsRecord = nestEggFixed(10000.0, 10, 15, 5)
@@ -52,6 +57,7 @@ def testNestEggFixed():
 # Problem 2
 #
 
+
 def nestEggVariable(salary, save, growthRates):
     """
     - salary: the amount of money you make each year.
@@ -62,12 +68,20 @@ def nestEggVariable(salary, save, growthRates):
     - return: a list of your retirement account value at the end of each year.
     """
 
-    savings = [] # List to save results
-    savings.append(salary * save * 0.01)
+    """
+    Formulae for calculation:
+    End of year 1 F[0] = salary * save * 0.01
+    End of year 2 F[1] = F[0] * (1 + 0.01 * growthRate) + salary * save * 0.01
+    End of year 3 F[2] = F[1] * (1 + 0.01 * growthRate) + salary * save * 0.01
+    """
+
+    savings = list([])  # List to save results
+    savings.append(salary * save * 0.01) # Add F[0]
     while len(savings) < len(growthRates):
         for i in range(1, len(growthRates)):
             savings.append(savings[i - 1] * (1 + 0.01 * growthRates[i]) + salary * save * 0.01)
     return savings
+
 
 def testNestEggVariable():
     salary = 10000
@@ -80,3 +94,81 @@ def testNestEggVariable():
 
     savingsRecord2 = nestEggVariable(15000, 10, [2, 3, 4, 5, 6, 7, 8, 9, 10, 11])
     print savingsRecord2
+
+#
+# Problem 3
+#
+
+
+def postRetirement(savings, growthRates, expenses):
+    """
+    - savings: the initial amount of money in your savings account.
+    - growthRate: a list of the annual percent increases in your investment
+      account (an integer between 0 and 100).
+    - expenses: the amount of money you plan to spend each year during
+      retirement.
+    - return: a list of your retirement account value at the end of each year.
+    """
+
+    """
+    Formulae for calculation
+    Retirement fund
+    End of year 1 F[0] = savings * (1 + 0.01 * growthRates[0]) – expenses
+    End of year 2 F[1] = F[0] * (1 + 0.01 * growthRates[1]) – expenses
+    End of year 3 F[2] = F[1] * (1 + 0.01 * growthRates[2]) – expenses
+    """
+    funds = list([])  # list for storing the value of funds
+    funds.append(savings * (1 + 0.01 * growthRates[0]) - expenses)  # Add F[0]
+    while len(funds) < len(growthRates):
+        for i in range(1, len(growthRates)):
+            funds.append(funds[i-1] * (1 + 0.01 * growthRates[i]) - expenses)
+    return funds
+
+
+def testPostRetirement():
+    savings     = 100000
+    growthRates = [10, 5, 0, 5, 1]
+    expenses    = 30000
+    savingsRecord = postRetirement(savings, growthRates, expenses)
+    print savingsRecord
+    # Output should have values close to:
+    # [80000.000000000015, 54000.000000000015, 24000.000000000015,
+    # -4799.9999999999854, -34847.999999999985]
+
+    raw_input()
+    savingsRecord2 = postRetirement(200000, [5, 5, 5, 5, 10, 12, 11], 50000)
+    print savingsRecord2
+
+
+#
+# Problem 4
+#
+
+def findMaxExpenses(salary, save, preRetireGrowthRates, postRetireGrowthRates,
+                    epsilon):
+    """
+    - salary: the amount of money you make each year.
+    - save: the percent of your salary to save in the investment account each
+      year (an integer between 0 and 100).
+    - preRetireGrowthRates: a list of annual growth percentages on investments
+      while you are still working.
+    - postRetireGrowthRates: a list of annual growth percentages on investments
+      while you are retired.
+    - epsilon: an upper bound on the absolute value of the amount remaining in
+      the investment fund at the end of retirement.
+    """
+
+
+def testFindMaxExpenses():
+    salary                = 10000
+    save                  = 10
+    preRetireGrowthRates  = [3, 4, 5, 0, 3]
+    postRetireGrowthRates = [10, 5, 0, 5, 1]
+    epsilon               = .01
+    expenses = findMaxExpenses(salary, save, preRetireGrowthRates,
+                               postRetireGrowthRates, epsilon)
+    print expenses
+    # Output should have a value close to:
+    # 1229.95548986
+
+    # TODO: Add more test cases here.
