d341695fa0455836c110b165982157d88ad7a68c
kronosjt <jithin10a@gmail.com>
2016-12-28 16:55:38 +0530

    Solution to Problem 1

--- ps5.py
+++ ps5.py
@@ -12,7 +12,8 @@ CONSONANTS = 'bcdfghjklmnpqrstvwxyz'
 HAND_SIZE = 7
 
 SCRABBLE_LETTER_VALUES = {
-    'a': 1, 'b': 3, 'c': 3, 'd': 2, 'e': 1, 'f': 4, 'g': 2, 'h': 4, 'i': 1, 'j': 8, 'k': 5, 'l': 1, 'm': 3, 'n': 1, 'o': 1, 'p': 3, 'q': 10, 'r': 1, 's': 1, 't': 1, 'u': 1, 'v': 4, 'w': 4, 'x': 8, 'y': 4, 'z': 10
+    'a': 1, 'b': 3, 'c': 3, 'd': 2, 'e': 1, 'f': 4, 'g': 2, 'h': 4, 'i': 1, 'j': 8, 'k': 5, 'l': 1, 'm': 3,
+    'n': 1, 'o': 1, 'p': 3, 'q': 10, 'r': 1, 's': 1, 't': 1, 'u': 1, 'v': 4, 'w': 4, 'x': 8, 'y': 4, 'z': 10
 }
 
 # -----------------------------------
@@ -75,7 +76,22 @@ def get_word_score(word, n):
     word: string (lowercase letters)
     returns: int >= 0
     """
-    # TO DO ...
+    # PSUEDOCODE:
+    # 1. initialize score to 0
+    # 2. if word is empty then score=0
+    # 3. else: for each letter in word, use letter as key & extract value from  SCRABBLE_LETTER_VALUES
+    # 4. if the lenth of word == n then add bonus 50
+
+    score = 0 # Initialize score
+
+    if len(word) != 0:
+        for letter in word:
+            score += SCRABBLE_LETTER_VALUES[letter]
+
+    if len(word) == n:
+        score += 50 # Bonus 50 for using all letters in the hand
+
+    return score
 
 #
 # Make sure you understand how this function works and what it does!
